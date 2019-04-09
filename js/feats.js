d52fb857e4e110520c66254bef9f23f838ed8ff6
Naksta <nak53188@gmail.com>
2019-04-08 23:42:38 -0400

    feats added

---
+++ js/feats.js
@@ -0,0 +1,15 @@
+(function() {
+    $('#result').on('click', 'li', function() {
+        var self = $(this);
+        var id =self.data('id');
+        location.href = 'feats.html?id=' + id;
+    });
+})();
+
+(function() {
+    $(document).ready(function(){
+        var id = url.substring(url.lastIndexOf('=') + 1);
+        var self = $(this);
+        var id =self.data('id');
+    })
+});
